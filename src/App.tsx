38053ac677d29cecde5f5d1faaaa31d5c0ebec3b
JD Angerhofer <jd@helloagora.com>
2020-06-20 00:54:55 -0400

    Demo of data masking with Apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC as RFC } from "react";
+import "./App.css";
+import { filter } from "graphql-anywhere";
+
+import { useQuery, gql, DocumentNode } from "@apollo/client";
+
+export type FC<props = {}> = RFC<props> & { fragment: DocumentNode };
 
 function App() {
+  const { data } = useQuery(gql`
+    query test {
+      user {
+        id
+        ...Component1
+        ...Component2
+      }
+    }
+
+    ${Component1.fragment}
+    ${Component2.fragment}
+  `);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Component1 user={filter(Component1.fragment, data?.user ?? {})} />
+      <Component2 user={filter(Component2.fragment, data?.user ?? {})} />
     </div>
   );
 }
 
 export default App;
+
+const Component1: FC<any> = (props) => (
+  <>
+    <h1>Component #1</h1>
+    <p>{JSON.stringify(props, null, 2)}</p>
+  </>
+);
+
+Component1.fragment = gql`
+  fragment Component1 on User {
+    lastName
+    activated
+  }
+`;
+
+const Component2: FC<any> = (props) => (
+  <>
+    <h1>Component #2</h1>
+    <p>{JSON.stringify(props, null, 2)}</p>
+  </>
+);
+
+Component2.fragment = gql`
+  fragment Component2 on User {
+    lastName
+    displayName
+    activated
+  }
+`;
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+import {
+  ApolloClient,
+  HttpLink,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: "https://dev.agora.construction/graphql",
+    headers: {
+      Authorization: "",
+    },
+  }),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
